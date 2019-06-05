5b2ce46b9463f77fb81da75c71027f1086ee049d
Sachin Umrao <troublemaker1512@gmail.com>
2019-06-06 02:36:43 +0530

    Data is loading and model gets trained. Now need to optimze the model

--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -2,60 +2,59 @@
 import pandas as pd
 import numpy as np
 import pmdarima as pm
-from yahoo_finance import Share
+import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 from matplotlib import style
-style.use("fivethirtyseven")
+import datetime
 
 # provide the details of stock to analyse
-stock_ticker = None
-start_date = None
-end_date = None
+stock_ticker = 'AAPL'
+
+start_year = 2015
+start_month = 1
+start_day = 1
+
+source = 'iex'
+
 forecast_horizon  = 7
 
-# if stock-ticker is not provided stop execution
-if stock_ticker is None:
-    print("Ticker is not provided. Stopping execution.")
-    exit()
-
-# if end-date is none, assume latest date
-if end_date is None:
-    end_date = None
-
-# if start-date is not given, use 01-Jan-2017 as start
-if start_date is None:
-    start_date = None
-
+start_date = datetime.datetime(start_year, start_month, start_day)
+end_date = datetime.datetime.now()
 
 # fetch data for stock
-stock = Share(stock_ticker)
-stock_data = stock.get_historical(start_date, end_date)
+stock = web.DataReader(stock_ticker, source, start_date, end_date)
 
-# parse stock data
+n = stock.shape[0]
+train_size = int(0.8*n)
 
 # divide closing stock prices into train-test set
+x_train = stock['close'][:train_size]
+x_test = stock['close'][train_size:]
 
 # fit ARIMA model on daily closing price
 model = pm.auto_arima(x_train)
 
 # produce forecast for test set using optimal ARIMA model
-forecast = model.score(x_test.shape[0])
+forecast = model.predict(x_test.shape[0])
 
 # calculate Mean Absolute Percentage Error (MAPE)
-mape = np.mean(np.abs(1 - x_train/x_test))
+mape = np.mean(np.abs(1 - forecast/x_test.values))
+print(mape)
 
 # find parameters for optimal model
 
 
 # train optimal model on complete dataset
-model_v2 = None
+#model_v2 = None
 
 # plot forecast
 plt.figure(figsize=(16,8))
-plt.plot(actual, 'g', lael='Actual')
+plt.plot(x_test.values, 'g', label='Actual')
 plt.plot(forecast, 'r', label='Forecast')
 plt.legend()
 plt.show()
 
 # TO-DO
-# Find likelihood for forecast
+# Optimze SARIMA model
+# Find variance for forecast
+# Improve the graph, support plotting of variance, date on x-axis
